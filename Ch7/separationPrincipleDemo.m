e7a7df816db967c8b50523a0b5c6d4c6ef771886
Dmitry Savransky <dsavransky@gmail.com>
2025-04-15 14:15:19 -0400

    tutorial and separation principle demo update

--- Ch7/separationPrincipleDemo.m
+++ Ch7/separationPrincipleDemo.m
@@ -1,4 +1,4 @@
-
+ 
 [x, y, z] = ellipsoid(0,5,0,2,1,1,20);
 c1 = 10;
 x = x+randn(size(x))/c1;
@@ -6,8 +6,8 @@ y = y+randn(size(y))/c1;
 z = z+randn(size(z))/c1;
 figure(1)
 clf
-%set(1,'Position', [100,265,560,420])
-set(1,'Position', [24,124,1140, 970])
+set(1,'Position', [100,265,560,420]) % small screen
+%set(1,'Position', [2562,2,1280,1335]) % large screen
 e1 = surf(x, y, z,'FaceLighting','phong','LineStyle','none','SpecularExponent',2);
 axis equal
 axlim = [-1,1,-1,1,-1,1]*8;
@@ -19,8 +19,8 @@ hold off
 
 figure(2)
 clf
-%set(2,'Position', [661,265,560,420])
-set(2,'Position', [1170,124,1140, 970])
+set(2,'Position', [661,265,560,420]) % small screen
+%set(2,'Position', [3843,2,1280,1335])% large screen
 e2 = surf(x, y, z,'FaceLighting','phong','LineStyle','none','SpecularExponent',2);
 axis equal
 hold on
@@ -30,6 +30,7 @@ axis([-3,3,2,8,-3,3])
 
 for j = 1:360*30
     rotate(e1,[30,0],1,[0,0,0])
+    rotate(e1,[30,60],1,[mean(e1.XData(:)),mean(e1.YData(:)),mean(e1.ZData(:))])
     rotate(e2,[30,60],1,[0,5,0])
     pause(0.01)
     
